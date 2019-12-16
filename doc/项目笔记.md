657afc96270a9b83b90a79632d683c2890cc8468
tym <oncwnuEMgsPnfGzAJcGGYyPs9l18@git.weixin.qq.com>
2019-12-16 16:55:22 +0800

    提交

---
+++ doc/项目笔记.md
@@ -0,0 +1,2 @@
+# picture_miniapp_backend
+微信小程序-照片排行
--- src/main/java/web/filter/AuthorizationInterceptor.java
+++ src/main/java/web/filter/AuthorizationInterceptor.java
@@ -1,10 +1,5 @@
 package web.filter;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTDecodeException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
